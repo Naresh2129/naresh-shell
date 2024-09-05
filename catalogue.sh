0bed455f239e839dcc22eda3eac6a30cf927111a
naresh <nareshboligorla@gmail.com>
2024-09-05 11:25:22 +0530

    catalogue service

--- catalogue.sh
+++ catalogue.sh
@@ -1,14 +1,5 @@
-cp /catalogue.service /etc/systemd/system/catalogue.service
-cp /mongo.repo //etc/yum.repos.d/mongo.repo
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
-dnf install nodejs -y
-useradd roboshop
-mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
-systemctl daemon-reload
-mongo --host mongodb.nkdevops29.online </app/schema/catalogue.js
+component=catalogue
+schema_type=mongodb
+source common.sh
+
+func_nodejs
